eaef8eee65eec84efd66cc219f6cc30e1a563814
Ryan Gadhi <gadi@gatech.edu>
2019-01-16 18:04:26 +0300

    added more elements in main activity

--- app/src/main/java/com/ryanplanet/projectx/chooseyourmajor/MainActivity.java
+++ app/src/main/java/com/ryanplanet/projectx/chooseyourmajor/MainActivity.java
@@ -3,6 +3,7 @@ package com.ryanplanet.projectx.chooseyourmajor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String Tag1 = "LifeCycle";
@@ -24,6 +25,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(Tag1,"on Resume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(Tag1,"on Pause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(Tag1,"on Stop");
+        Toast.makeText(getBaseContext(), "We will wait for you!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(Tag1,"on Restart");
+
+        Toast.makeText(getBaseContext(), "Welcome Back!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(Tag1,"on Destroy");
+
+
+//   Does not work
+//        Toast.makeText(getBaseContext(), "Bye Bye!",
+//                Toast.LENGTH_LONG).show();
+
 
     }
 }
